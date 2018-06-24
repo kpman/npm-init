eed02741f885038d2ca9096ad680d3bef8a62313
kpman <s92f002@hotmail.com>
2018-06-24 13:54:55 +0800

    Use gitconfig

--- npm-init.js
+++ npm-init.js
@@ -112,12 +112,129 @@ files.forEach(file => {
   }
 });
 
+/*
+  decode function and required functions copied from
+  https://github.com/npm/ini
+  in order to parse github config without added reqs
+*/
+
+/* eslint-disable */
+function dotSplit(str) {
+  return str
+    .replace(/\1/g, '\u0002LITERAL\\1LITERAL\u0002')
+    .replace(/\\\./g, '\u0001')
+    .split(/\./)
+    .map(function(part) {
+      return part
+        .replace(/\1/g, '\\.')
+        .replace(/\2LITERAL\\1LITERAL\2/g, '\u0001');
+    });
+}
+
+function decode(str) {
+  var out = {};
+  var p = out;
+  var section = null;
+  //          section     |key      = value
+  var re = /^\[([^\]]*)\]$|^([^=]+)(=(.*))?$/i;
+  var lines = str.split(/[\r\n]+/g);
+
+  lines.forEach(function(line, _, __) {
+    if (!line || line.match(/^\s*[;#]/)) return;
+    var match = line.match(re);
+    if (!match) return;
+    if (match[1] !== undefined) {
+      section = unsafe(match[1]);
+      p = out[section] = out[section] || {};
+      return;
+    }
+    var key = unsafe(match[2]);
+    var value = match[3] ? unsafe(match[4] || '') : true;
+    switch (value) {
+      case 'true':
+      case 'false':
+      case 'null':
+        value = JSON.parse(value);
+    }
+
+    // Convert keys with '[]' suffix to an array
+    if (key.length > 2 && key.slice(-2) === '[]') {
+      key = key.substring(0, key.length - 2);
+      if (!p[key]) {
+        p[key] = [];
+      } else if (!Array.isArray(p[key])) {
+        p[key] = [p[key]];
+      }
+    }
+
+    // safeguard against resetting a previously defined
+    // array by accidentally forgetting the brackets
+    if (Array.isArray(p[key])) {
+      p[key].push(value);
+    } else {
+      p[key] = value;
+    }
+  });
+
+  // {a:{y:1},"a.b":{x:2}} --> {a:{y:1,b:{x:2}}}
+  // use a filter to return the keys that have to be deleted.
+  Object.keys(out)
+    .filter(function(k, _, __) {
+      if (!out[k] || typeof out[k] !== 'object' || Array.isArray(out[k])) {
+        return false;
+      }
+      // see if the parent section is also an object.
+      // if so, add it to that, and mark this one for deletion
+      var parts = dotSplit(k);
+      var p = out;
+      var l = parts.pop();
+      var nl = l.replace(/\\\./g, '.');
+      parts.forEach(function(part, _, __) {
+        if (!p[part] || typeof p[part] !== 'object') p[part] = {};
+        p = p[part];
+      });
+      if (p === out && nl === l) {
+        return false;
+      }
+      p[nl] = out[k];
+      return true;
+    })
+    .forEach(function(del, _, __) {
+      delete out[del];
+    });
+
+  return out;
+}
+
+function getGitConfig(gitConfFile) {
+  var exists, conf;
+  exists = fs.existsSync(gitConfFile);
+  conf = {
+    user: {
+      name: '',
+      email: '',
+    },
+  };
+  if (exists) {
+    conf = decode(fs.readFileSync(gitConfFile, 'utf-8'));
+  } else {
+    console.log('\nGit configuration file does not exist!!!...\n');
+  }
+  return conf;
+}
+/* eslint-enable */
+
+const homeDir =
+  process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+const gitConfig = path.join(homeDir, '.gitconfig');
+const git = getGitConfig(gitConfig);
+
 module.exports = {
   name,
   description: prompt('An awesome project description...', ''),
   author: {
-    name: 'kpman',
-    email: 's92f002@gmail.com',
+    name: git.user.name,
+    email: git.user.email,
   },
   license: 'MIT',
   version: '0.0.0',
